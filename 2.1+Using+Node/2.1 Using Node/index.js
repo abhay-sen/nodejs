ef1ff6c7274121f6d8d66f6d7563e12a1790cd9f
abhay-sen <abhaysen0502@gmail.com>
2024-03-21 01:29:23 +0530

    first commit

---
+++ 2.1+Using+Node/2.1 Using Node/index.js
@@ -0,0 +1,5 @@
+console.log("hello");
+const add = (a, b) => {
+    return a + b
+}
+console.log(add(4, 6))
---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,6 @@
+
+import generateName from "sillyname";
+import superheroes from "superheroes";
+var sillyName = generateName();
+console.log(`my name is ${sillyName}`);
+console.log(`my superhero name is ${superheroes.random()}`);
