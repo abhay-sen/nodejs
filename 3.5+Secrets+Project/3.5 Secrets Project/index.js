204a9fdf230cbeaf268adf5b6c44ec663aba2373
abhay-sen <abhaysen0502@gmail.com>
2024-03-22 00:26:18 +0530

    hmm

--- 3.5+Secrets+Project/3.5 Secrets Project/index.js
+++ 3.5+Secrets+Project/3.5 Secrets Project/index.js
@@ -1,3 +1,36 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var result = "";
+app.use(bodyParser.urlencoded({extended:true}));
+function validate(req,res,next){
+    
+    if(req.body["password"]=="iloveprogramming"){
+        result = "/public/secret.html";
+    }
+    else{
+        result = "/public/index.html";
+    }
+    
+    next();
+
+}
+app.use(validate);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/check",(req,res)=>{
+    res.sendFile(__dirname+ result);
+
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
